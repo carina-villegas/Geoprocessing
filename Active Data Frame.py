cd482c8a1ef18fce44462dd32478aa73b0b10b73
Carina Villegas <120668515+carina-villegas@users.noreply.github.com>
2022-12-27 21:15:55 +0100

    Update Active Data Frame.py

--- Active Data Frame.py
+++ Active Data Frame.py
@@ -7,3 +7,27 @@ for item in maps:
     myMxd.saveACopy(r'C:\Data\Results'+item.name+'.mxd')
 del myMxd
 print "Done"
+
+#For naming all visible layers
+
+import arcpy.mapping as c
+myMxd=c.MapDocument(r'C:\Data\Results\UUEE_2.mxd')
+layers=c.ListLayers(myMxd)
+for item in layers:
+    if item.visible:
+        print item.name
+print "Done"
+
+#To change visibility of layers
+
+import arcpy.mapping as c
+myMxd=c.MapDocument(r'C:\Data\Results\UUEE_2.mxd')
+layers=c.ListLayers(myMxd)
+for item in layers:
+    if item.visible:
+        item.visible=False
+    else:
+        item.visible=True
+        print item.name
+myMxd.save()
+print "Done"
